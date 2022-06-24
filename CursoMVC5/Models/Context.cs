a39ed1bbda77c8c2237c433979e49b38272e9ef1
Thiago-AM-Souza <thiagosouza96.ts@gmail.com>
2022-06-24 15:53:36 -0300

    initial commit

---
+++ CursoMVC5/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC5.Models
+{
+    public class Context: DbContext
+    {
+        public virtual DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString:@"Server = localhost\SQLEXPRESS; Database = master; Trusted_Connection = True;");
+        }
+    }
+}
